15b4b0736632b7c92c0b4e89403fe59ac0c90a65
BeigeSponge <michaelleary66@gmail.com>
2024-06-06 21:31:03 +0100

    Changed requirements

--- setup.py
+++ setup.py
@@ -2,23 +2,32 @@ from setuptools import setup, find_packages
 
 
 def readme():
-    with open('README.md', encoding="utf-8") as f:
+    with open("README.md", encoding="utf-8") as f:
         return f.read()
 
-setup(name='chwrapper',
-      version='0.3.0',
-      description='A simple wrapper around the Companies House API',
-      long_description=readme(),
-      url='http://github.com/jamesgardiner/chwrapper',
-      author='James Gardiner',
-      author_email='jamesg87@me.com',
-      license='MIT',
-      packages=find_packages(),
-      zip_safe=False,
-      install_requires=[
-          'requests==2.8.1',
-      ],
-      classifiers=[
+
+setup(
+    name="chwrapper",
+    version="0.3.0",
+    description="A simple wrapper around the Companies House API",
+    long_description=readme(),
+    url="http://github.com/jamesgardiner/chwrapper",
+    author="James Gardiner",
+    author_email="jamesg87@me.com",
+    license="MIT",
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=[
+        "cookies",
+        "coverage",
+        "py",
+        "pytest",
+        "pytest-cov",
+        "requests",
+        "responses",
+        "wheel",
+    ],
+    classifiers=[
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2",
@@ -28,4 +37,5 @@ setup(name='chwrapper',
         "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
-      ])
+    ],
+)
